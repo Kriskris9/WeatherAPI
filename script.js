9969fd8f74d18d5a87e85767bcbb83ac50a81103
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-03-29 23:29:21 -0700

    got function fiveDay working

--- script.js
+++ script.js
@@ -6,26 +6,30 @@ var city = document.getElementById("city");
 var searchHistory = [];
 
 
+
 function cityWeather(event) {
 
     console.log(city);
     console.log(city.value);
     event.preventDefault();
     showRecentSearch();
-    fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&units=imperial&appid=d0e531028fc8f0dc428b9a2ef2bcbf24&cnt=5`) 
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&units=imperial&appid=d0e531028fc8f0dc428b9a2ef2bcbf24&cnt=5`)
         .then(function (response) {
             return response.json();
-            console.log(response);
         })
         .then(function (data) {
+            console.log(data);
             var date = new Date(data.list[0].dt * 1000).toLocaleDateString();
             var cityName = data.city.name;
             var temp = data.list[0].main.temp;
-             var humidity = data.list[0].main.humidity;
+            var humidity = data.list[0].main.humidity;
             var windSpeed = data.list[0].wind.speed;
             var icon = data.list[0].weather[0].icon;
             var dateText = data.list[0].dt_txt;
+            var lat = data.city.coord.lat;
+            var lon = data.city.coord.lon;
 
+            fiveDay(lat,lon)
             displayWeatherData(cityName, date, temp, humidity, windSpeed, icon);
 
 
@@ -34,12 +38,13 @@ function cityWeather(event) {
         .catch(function error(error) {
             console.error(error);
         });
+
 }
- 
+
 
 search.addEventListener("click", cityWeather)
 
-function displayWeatherData (cityName, date, temp, humidity, windSpeed, icon) {
+function displayWeatherData(cityName, date, temp, humidity, windSpeed, icon) {
     var currentWeatherEl = document.querySelector('.recent-search');
     var iconUrl = `https://openweathermap.org/img/w/${icon}.png`
     currentWeatherEl.innerHTML = `
@@ -49,18 +54,37 @@ function displayWeatherData (cityName, date, temp, humidity, windSpeed, icon) {
          <p>Wind Speed: ${windSpeed} MPH</p>
        `;
     searchResult.appendChild(currentWeatherEl);
-    
+
 }
- 
 
 
-    function showRecentSearch() {
-        var history = document.querySelector('.history');
-        var cities = document.createElement('li');
-        cities.textContent = city.value;
-        history.appendChild(cities);
-        searchHistory.push(city.value);
-        localStorage.setItem('city', searchHistory);
-    }
 
+function showRecentSearch() {
+    var history = document.querySelector('.history');
+    var cities = document.createElement('li');
+    cities.textContent = city.value;
+    history.appendChild(cities);
+    searchHistory.push(city.value);
+    localStorage.setItem('city', searchHistory);
+
+
+}
+
+
+function fiveDay(lat, lon) {
+    
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=d0e531028fc8f0dc428b9a2ef2bcbf24`)
+        .then(function (response) {
+            return response.json();
+            console.log(response);
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+
+        .catch(function error(error) {
+            console.error(error);
+        });
+
+}
 
